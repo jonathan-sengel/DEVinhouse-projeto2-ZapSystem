c4f4db0fbf9ed03f9965ed6f42bb016fe6d06478
Jonathan Engel <joow.engeel@gmail.com>
2021-09-13 21:55:48 -0300

    ajustes

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,16 @@
-import GridArea from "./components/GridArea";
-import Header from "./components/Header";
-import Aside from "./components/Aside";
-import Main from "./components/Main";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import NotFoundPage from "./pages/NotFoundPage";
+import ZapSystemPage from "./pages/ZapSystemPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/">
-          <GridArea>
-            <Header />
-            <Aside />
-            <Main />
-          </GridArea>
+        <Route path="/" exact>
+          <ZapSystemPage />
         </Route>
         <Route>
-          <h1 style={{ color: "#fff", textAlign: "center" }}>404 Não Encontrado!</h1>
+          <NotFoundPage />
         </Route>
       </Switch>
     </BrowserRouter>
---
+++ src/pages/ZapSystemPage.jsx
@@ -0,0 +1,16 @@
+import GridArea from "../components/GridArea";
+import Header from "../components/Header";
+import Aside from "../components/Aside";
+import Main from "../components/Main";
+
+const ZapSystemPage = () => {
+  return (
+    <GridArea>
+      <Header />
+      <Aside />
+      <Main />
+    </GridArea>
+  );
+};
+
+export default ZapSystemPage;
